e7052b72a6f6ffe108752143c9253e5ffea83638
Shakillazycoder <sakil.khan882.sk@gmail.com>
2024-01-27 17:12:07 +0600

    2task done

---
+++ 1task.js
@@ -0,0 +1,8 @@
+burger = 400;
+coke = 30;
+if ( burger > 500){
+    console.log('free coke')
+}
+else{
+    console.log(coke)
+}
---
+++ 2task.js
@@ -0,0 +1,21 @@
+BMI = 20;
+// if(BMI <18.5){
+//     console.log('you are underweight')
+// }
+// else{
+//     console.log('you are obese')
+// }
+
+// if (BMI >=18.5 && BMI <=24.9){
+//     console.log('you are normal')
+// }
+// else{
+//     console.log('you are obese')
+// }
+
+if (BMI >=25 && BMI <=29.9){
+    console.log('you are normal')
+}
+else{
+    console.log('you are obese')
+}
