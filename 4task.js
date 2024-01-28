605256a503e182dba459428aaa124b32299e18ad
Shakillazycoder <sakil.khan882.sk@gmail.com>
2024-01-28 18:36:08 +0600

    task finished

---
+++ 4task.js
@@ -0,0 +1,18 @@
+const score = 39;
+if ( score >80){
+    console.log ('Go for lunch')
+}
+else{
+    if(score < 80 && score >=60){
+        console.log('good luck next time')
+    } 
+    else if( score <60 && score >=40){
+        console.log('keep your friends message unseen')
+    }
+    else if ( score <40){
+        console.log('block your friend')
+    }
+    else if ( score <80){
+        console.log('home and sleep and act sad')
+    }
+}
---
+++ 5task.js
@@ -0,0 +1,15 @@
+let num1 = 10;
+let num2 =5;
+// let result;
+// if (num1 > num2){
+//     result = num1 *2;
+// }
+// else{
+//     result = num1 + num2;
+// }
+// console.log(result)
+
+// ternary operator
+
+let result = (num1 > num2)? num1 * 2: num1 + num2;
+console.log(result)
