d7c50ca8cfe594f438b422776e6eae4ef398343e
Shakillazycoder <sakil.khan882.sk@gmail.com>
2024-01-28 20:52:34 +0600

    3task done again with a different style

---
+++ concept.js
@@ -0,0 +1,9 @@
+var x = 4.658737;
+console.log(typeof x);
+
+var convert = x.toFixed(2);
+console.log(convert);
+console.log(typeof convert);
+
+console.log (parseInt(convert))
+console.log(typeof parseInt(convert))
